930dd3939f488ff4e66679fd8fd053917478542f
shraddhaphadnis <phadnis.s@husky.neu.edu>
2017-04-03 16:21:06 -0400

    changes in server.js to support project

--- web-dev-template/server.js
+++ web-dev-template/server.js
@@ -1,9 +1,23 @@
 var express = require('express');
+// var passport      = require('passport');
+// var cookieParser  = require('cookie-parser');
+// var session       = require('express-session');
+
 var app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(session({
+//     secret: 'this is the secret',
+//     resave: true,
+//     saveUninitialized: true
+// }));
+//
+// app.use(cookieParser());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -11,8 +25,10 @@ app.use(express.static(__dirname + '/public'));
 require ("./test/app.js")(app);
 // require("./assignment-wed/app.js")(app);
 
-var assignment = require("./assignment/app.js");
-assignment(app);
+//var assignment = require("./assignment/app.js");
+var project = require("./project/app.js");
+//assignment(app);
+project(app);
 
 var port      = process.env.PORT || 3000;
 
