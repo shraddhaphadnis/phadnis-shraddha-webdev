43453482d9a22d8128e43b3e1c2ddb388e4244bb
shraddhaphadnis <phadnis.s@husky.neu.edu>
2017-04-03 16:26:43 -0400

    additional changes to fix project error

--- web-dev-template/public/server.js
+++ web-dev-template/public/server.js
@@ -9,11 +9,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 var assignment = require("./assignment/app.js");
+var project = require("./project/client/js/app");
 require("./assignment/model/user/user.model.server.js");
 require("./assignment/model/website/website.model.server.js");
 require("./assignment/model/page/page.model.server.js");
 require("./assignment/model/widget/widget.model.server.js");
+require("../project/models/user/user.model.server");
+require("../project/models/hotel/hotel.model.server");
+require("../project/models/review/review.model.server");
+require("../project/models/city/city.model.server");
 assignment(app);
+project(app);
 
 var port      = process.env.PORT || 3000;
 
